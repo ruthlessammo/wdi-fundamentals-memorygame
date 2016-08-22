480e69984c66746cf19cd32adc3f2c3b0974f4c8
Will Hilton <williamjchilton@gmail.com>
2016-08-22 17:41:08 +0100

    Update main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,17 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+if (cardOne === cardTwo) {
+  alert("You found a match!");
+} else if (cardOne != cardThree) {
+  alert("Sorry try again");
+} else if (cardOne != cardFour) {
+  alert("Sorry try again");
+} else if (cardTwo != cardThree) {
+  alert("Sorry try again");
+} else if (cardTwo != cardFour) {
+  alert("Sorry try again");
+} else if (cardThree === cardFour) {
+	alert("You found a match!");
+} 
