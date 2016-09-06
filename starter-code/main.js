95330b712a39f227c58af0d9cc82e5013b99d337
williamhilton <williamhilton@tuta.io>
2016-09-06 15:24:05 +0100

    Cards been added by javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,17 +1,24 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-if (cardOne === cardTwo) {
-  alert("You found a match!");
-} else if (cardOne != cardThree) {
-  alert("Sorry try again");
-} else if (cardOne != cardFour) {
-  alert("Sorry try again");
-} else if (cardTwo != cardThree) {
-  alert("Sorry try again");
-} else if (cardTwo != cardFour) {
-  alert("Sorry try again");
-} else if (cardThree === cardFour) {
-	alert("You found a match!");
+//var cardOne = "queen";
+//var cardTwo = "queen";
+//var cardThree = "king";
+//var cardFour = "king";
+//if (cardOne === cardTwo) {
+//  alert("You found a match!");
+//} else {
+//alert("Sorry, try again");
+//};
+var board = document.getElementById('game-board');
+
+function createBoard() {
+
+	for (var i = 0; i < 4; i++) {
+
+		var cardElement = document.createElement('div');
+
+		cardElement.className = 'card';
+		
+		board.appendChild(cardElement);
+	}
 } 
+createBoard();
+
